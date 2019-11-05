758afad58b54578aeeb733c8e3e4c5bab2f88d58
RomanHurman <roman.gurman27@gmail.com>
2019-11-05 10:34:12 +0200

    new market

---
+++ script.js
@@ -0,0 +1,167 @@
+// const newWrapper = document.createElement("div");
+// newWrapper.innerText = "Market";
+// newWrapper.id = "new_wrapper";
+// newWrapper.style.display = "flex";
+// newWrapper.style.justifyContent = "center";
+// newWrapper.style.alignItems = "center";
+// newWrapper.style.border = "1px solid black";
+// newWrapper.style.height = "700px";
+// newWrapper.style.width = "800px";
+// newWrapper.style.marginLeft = "600px";
+// newWrapper.style.marginTop = "100px";
+// document.body.append(newWrapper)
+//
+//
+// const newBut = document.createElement("button");
+// newBut.innerText  = "buy";
+// newBut.className = "new_but";
+// newWrapper.append(newBut)
+//
+// newInput = document.createElement("input");
+// newInput.type = "text";
+// newInput.placeholder = "Do your choice";
+// newInput.className = "new_Input";
+// newWrapper.append(newInput)
+//
+// newButt = document.createElement("button");
+// newButt.innerText  = "weight";
+// newButt.className = "new_butt";
+// newWrapper.append(newButt)
+//
+// const newRow = document.createElement("span");
+// newRow.innerText = "";
+// newRow.className = "new_Row";
+// newWrapper.append(newRow)
+//
+//
+// class Market {
+//     constructor(name, weight){
+//         this.name = name;
+//         this.weight = weight;
+//     }
+//     find(){
+//         const item = ["banana","orange", "apple"];
+//         item.forEach()
+//     });
+// }
+// }
+//
+// const items1 = new Market("Pasta", 3);
+// console.log(items1);
+// items1.find()
+//
+// const items1 = new Market("Pasta", 3, 40, "Grocery Department");
+// console.log(items1);
+// const items2 = new Market("Sausages", 30, 200, "Meat Department");
+// console.log(items2)
+// const items3 = new Market("Sardine", 35, 300, "Fish Department");
+// console.log(items3)
+// const items4 = new Market("Potato", 3, 40, "Vegetables Department");
+// console.log(items4);
+// const items5 = new Market("Tomato", 30, 200, "Vegetables Department");
+// console.log(items5)
+// const items6 = new Market("Cucumber", 35, 300, "Vegetables Department");
+// console.log(items6)
+// const items7 = new Market("Liver", 3, 40, "Meat Department");
+// console.log(items7);
+// const items8 = new Market("Bacon", 30, 200, "Meat Department");
+// console.log(items8)
+// const items9 = new Market("Turkey", 35, 300, "Meat Department");
+// console.log(items9)
+// const items10 = new Market("Orange", 3, 40, "Fruit Departament");
+// console.log(items10);
+// const items11 = new Market("Banana", 30, 200, "Fruit Departament");
+// console.log(items11)
+// const items12 = new Market("Tea", 35, 300, "Drinks Department");
+// console.log(items12)
+// const items13 = new Market("Coffee", 35, 300, "Drinks Department");
+// console.log(items13)
+// const items14 = new Market("Parmesan", 35, 300, "Delicatessen Department");
+// console.log(items14)
+// const items15 = new Market("Baquette", 35, 300, "Breads and flours Department");
+// console.log(items15)
+// const items16 = new Market("Yogurt", 35, 300, "Dairy produce Department");
+// console.log(items16)
+
+// function myFunction() {
+//     document.body.textContent = "ewfwefwefewfewfewfewfewf";
+//     console.log("Food" + " " + "Name" + ":" + f1.name);
+//     console.log("Food" + " " + "Name" + ":" + f2.name);
+//     console.log("Food" + " " + "Name" + ":" + f3.name);
+//     console.log("Food" + " " + "Name" + ":" + f4.name);
+//     console.log("Food" + " " + "Name" + ":" + f5.name);
+//     console.log("Food" + " " + "Name" + ":" + f6.name);
+//     console.log("Food" + " " + "Name" + ":" + f7.name);
+//     console.log("Food" + " " + "Name" + ":" + f8.name);
+//     console.log("Food" + " " + "Name" + ":" + f9.name);
+//     console.log("Food" + " " + "Name" + ":" + f10.name);
+//     console.log("Food" + " " + "Name" + ":" + f11.name);
+//     console.log("Food" + " " + "Name" + ":" + f12.name);
+//     console.log("Food" + " " + "Name" + ":" + f13.name);
+//     console.log("Food" + " " + "Name" + ":" + f14.name);
+//     console.log("Food" + " " + "Name" + ":" + f15.name);
+// }
+//
+// function myFunc() {
+//     console.log("Orange" + " " + "Weight" + ":" + f1.weight + " " + "Kg");
+//     console.log("Apple" + " " + "Weight" + ":" + f2.weight + " " + "Kg");
+//     console.log("Pasta" + " " + "Weight" + ":" + f3.weight + " " + "Kg");
+//     console.log("Sausages" + " " + "Weight" + ":" + f4.weight + " " + "Kg");
+//     console.log("Sardine" + " " + "Weight" + ":" + f5.weight + " " + "Kg");
+//     console.log("Potato" + " " + "Weight" + ":" + f6.weight + " " + "Kg");
+//     console.log("Tomato" + " " + "Weight" + ":" + f7.weight + " " + "Kg");
+//     console.log("Cucumber" + " " + "Weight" + ":" + f8.weight + " " + "Kg");
+//     console.log("Liver" + " " + "Weight" + ":" + f9.weight + " " + "Kg");
+//     console.log("Bacon" + " " + "Weight" + ":" + f10.weight + " " + "Kg");
+//     console.log("Turkey" + " " + "Weight" + ":" + f11.weight + " " + "Kg");
+//     console.log("Orange" + " " + "Weight" + ":" + f12.weight + " " + "Kg");
+//     console.log("Banana" + " " + "Weight" + ":" + f13.weight + " " + "Kg");
+//     console.log("Tea" + " " + "Weight" + ":" + f14.weight + " " + "Kg");
+//     console.log("Coffee" + " " + "Weight" + ":" + f15.weight + " " + "Kg");
+// }
+//
+// function myFun() {
+//     console.log("Orange" + " " + "Price" + ":" + f1.price + " " + "Uah");
+//     console.log("Apple" + " " + "Price" + ":" + f2.price + " " + "Uah");
+//     console.log("Pasta" + " " + "Price" + ":" + f3.price + " " + "Uah");
+//     console.log("Sausages" + " " + "Price" + ":" + f4.price + " " + "Uah");
+//     console.log("Sardine" + " " + "Price" + ":" + f5.price + " " + "Uah");
+//     console.log("Potato" + " " + "Price" + ":" + f6.price + " " + "Uah");
+//     console.log("Tomato" + " " + "Price" + ":" + f7.price + " " + "Uah");
+//     console.log("Cucumber" + " " + "Price" + ":" + f8.price + " " + "Uah");
+//     console.log("Liver" + " " + "Price" + ":" + f9.price + " " + "Uah");
+//     console.log("Bacon" + " " + "Price" + ":" + f10.price + " " + "Uah");
+//     console.log("Turkey" + " " + "Price" + ":" + f11.price + " " + "Uah");
+//     console.log("Orange" + " " + "Price" + ":" + f12.price + " " + "Uah");
+//     console.log("Banana" + " " + "Price" + ":" + f13.price + " " + "Uah");
+//     console.log("Tea" + " " + "Price" + ":" + f14.price + " " + "Uah");
+//     console.log("Coffee" + " " + "Price" + ":" + f15.price + " " + "Uah");
+// }
+
+// function myFunP() {
+//     console.log(f1.place + " " + "Top box");
+// }
+
+// const newButt = document.createElement("button");
+// newButt.innerText = "Weight";
+// newButt.className = "new_Butt";
+// Wrapper.append(newButt);
+// newButt.addEventListener("click",() => {
+//     myFunc()
+// })
+//
+// const newBut = document.createElement("button");
+// newBut.innerText = "Price";
+// newBut.className = "new_But";
+// Wrapper.append(newBut);
+// newBut.addEventListener("click",() => {
+//     myFun()
+// })
+//
+// const newButP = document.createElement("button");
+// newButP.innerText = "Buy";
+// newButP.className = "new_ButP";
+// Wrapper.append(newButP);
+// newBut.addEventListener("click",() => {
+//     myFunP()
+// })
